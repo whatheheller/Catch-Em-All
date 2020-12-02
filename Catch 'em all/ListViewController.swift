cd5514e8563d2ad55bb84d4288fc9ccc9782f7ba
Alex <amheller@gmail.com>
2020-12-01 17:11:33 -0800

    Completed 7.3

--- Catch 'em all/ListViewController.swift
+++ Catch 'em all/ListViewController.swift
@@ -7,20 +7,60 @@ class ListViewController: UIViewController {
     @IBOutlet weak var tableView: UITableView!
     
     var creatures = Creatures()
+    var activityIndicator = UIActivityIndicatorView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.delegate = self
         tableView.dataSource = self
         
+        setupActivityIndicator()
+        
+        activityIndicator.startAnimating()
+        self.view.isUserInteractionEnabled = false
+        
         creatures.getData {
             DispatchQueue.main.async {
+                self.navigationItem.title = "\(self.creatures.creatureArray.count) of \(self.creatures.count) Pokemon"
                 self.tableView.reloadData()
+                self.activityIndicator.stopAnimating()
+                self.view.isUserInteractionEnabled = true
+            }
+        }
+    }
+    
+    func setupActivityIndicator() {
+        activityIndicator.center = self.view.center
+        activityIndicator.hidesWhenStopped = true
+        activityIndicator.style = .large
+        activityIndicator.color = .red
+        view.addSubview(activityIndicator)
+    }
+    
+    func loadAll() {
+        if creatures.urlString.hasPrefix("http") {
+            creatures.getData {
+                DispatchQueue.main.async {
+                    self.navigationItem.title = "\(self.creatures.creatureArray.count) of \(self.creatures.count) Pokemon"
+                    self.tableView.reloadData()
+                }
+                self.loadAll()
+            }
+        } else {
+            DispatchQueue.main.async {
+                print("All pokemon loaded. Total count is \(self.creatures.creatureArray.count)")
+                self.activityIndicator.stopAnimating()
+                self.view.isUserInteractionEnabled = true
             }
         }
     }
 
-
+    @IBAction func loadAllPressed(_ sender: UIBarButtonItem) {
+        activityIndicator.startAnimating()
+        self.view.isUserInteractionEnabled = false
+        loadAll()
+    }
+    
 }
 
 extension ListViewController: UITableViewDelegate, UITableViewDataSource {
@@ -29,11 +69,15 @@ extension ListViewController: UITableViewDelegate, UITableViewDataSource {
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        print("\(indexPath.row) of \(creatures.creatureArray.count)")
         if indexPath.row == creatures.creatureArray.count-1 && creatures.urlString.hasPrefix("http") {
+            activityIndicator.startAnimating()
+            self.view.isUserInteractionEnabled = false
             creatures.getData {
                 DispatchQueue.main.async {
+                    self.navigationItem.title = "\(self.creatures.creatureArray.count) of \(self.creatures.count) Pokemon"
                     self.tableView.reloadData()
+                    self.activityIndicator.stopAnimating()
+                    self.view.isUserInteractionEnabled = true
                 }
             }
         }
